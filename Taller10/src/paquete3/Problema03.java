21dfea7c8eef753cc033b6760189307d468f2e74
danyberna2002@gmail.com <danyberna2002@gmail.com>
2025-12-18 12:39:55 -0500

    Problema3

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,22 +10,33 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
+
     public static void main(String[] args) {
-        
-    boolean[][] sensores = {
-    {true, true, true, true, true, true},
-    {true, false, true, false, true, false},
-    {false, false, false, false, false, false},
-    {true, true, false, true, true, false}
-};
-    int[] conteo = new int[sensores.length];
-    for (int i = 0; i < sensores.length; i++) {
+
+        boolean[][] sensores = {
+            {true, true, true, true, true, true},
+            {true, false, true, false, true, false},
+            {false, false, false, false, false, false},
+            {true, true, false, true, true, false}
+        };
+        int[] conteo = new int[sensores.length];
+        for (int i = 0; i < sensores.length; i++) {
             int sensores_funcionales = 0;
             for (int j = 0; j < sensores[i].length; j++) {
                 if (sensores[i][j]) {
                     sensores_funcionales++;
                 }
             }
-    
-}
+            conteo[i] = i + 1;
+
+            if (sensores_funcionales == 6) {
+                System.out.printf("%s zonas estan funcionando sin ningun "
+                        + "tipo de fallo\n", conteo[i]);
+            } else {
+                System.out.printf("La zona %s tienen sensores con fallos\n",
+                        conteo[i]);
+            }
+        }
+
+    }
 }
