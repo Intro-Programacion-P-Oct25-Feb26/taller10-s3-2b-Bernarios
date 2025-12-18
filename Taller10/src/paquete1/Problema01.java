4906567a213e549fb871a377491d05d75b693c66
danyberna2002@gmail.com <danyberna2002@gmail.com>
2025-12-18 11:10:28 -0500

    Problema1

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,6 +15,36 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        boolean[][] asistencia = {
+            {true, true, false, true, true},
+            {true, false, false, true, false},
+            {true, true, true, true, true},
+            {false, true, true, false, true}
+        };
+
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+
+        int[] conteo_asistencias = new int[estudiantes.length];
+
+        for (int i = 0; i < asistencia.length; i++) {
+            int dias_asistidos = 0;
+            for (int j = 0; j < asistencia[i].length; j++) {
+                if (asistencia[i][j]) {
+                    dias_asistidos++;
+                }
+            }
+
+            conteo_asistencias[i] = dias_asistidos;
+
+            System.out.printf("Estudiante:%s tiene %s asistencias ",
+                     estudiantes[i], conteo_asistencias[i]);
+
+            if (conteo_asistencias[i] == 5) {
+                System.out.println("por lo tanto SI cumple con todas sus "
+                        + "asistencias");
+            } else {
+                System.out.println("por lo tanto NO cumple con las asistencias");
+            }
+        }
     }
-    
 }
