414b67050d8f47f186371a4aac13d924cb4c0d75
danyberna2002@gmail.com <danyberna2002@gmail.com>
2025-12-18 20:35:11 -0500

    Problema06

--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -5,10 +5,48 @@
  */
 package paquete6;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema06 {
 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double[][] matriz = new double[5][6];
+        double[][] matriz_resultante = new double[5][6];
+        int multi;
+        System.out.println("ingrese el multiplicador");
+        multi = entrada.nextInt();
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("Ingrese los valores a multiplicar en "
+                        + "la posicion %s %s\n", i, j);
+                matriz[i][j] = entrada.nextDouble();
+
+            }
+        }
+        for (int i = 0; i < matriz_resultante.length; i++) {
+            for (int j = 0; j < matriz_resultante[i].length; j++) {
+                matriz_resultante[i][j] = matriz[i][j] * multi;
+            }
+        }
+        System.out.println("Los valores ingresados son:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.printf("%s\t", matriz[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("los valores resultantes son:");
+        for (int i = 0; i < matriz_resultante.length; i++) {
+            for (int j = 0; j < matriz_resultante[i].length; j++) {
+                System.out.printf("%s\t", matriz_resultante[i][j]);
+            }
+            System.out.println("");
+        }
+    }
 }
